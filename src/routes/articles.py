02a6ed14d7274982145d634ba1be920ff56e5960
slaughtbear <ivanaguirre368@gmail.com>
2025-05-09 08:26:54 -0600

    Add endpoint to get one article by id

--- src/routes/articles.py
+++ src/routes/articles.py
@@ -20,3 +20,31 @@ def get_all_articles() -> list[Article]:
             detail = "No se encontró ninguna publicación creada en la base de datos"
         )
     return articles.data
+
+@articles_router.get("/{id}")
+def get_one_article(id: int) -> Article:
+    """
+    Endpoint de tipo GET para obtener una publicación buscando
+    por id en la base de datos
+
+    returns:
+        Article: Publicación
+    """
+    article = search_article(id)
+    if not article:
+        raise HTTPException(
+            status_code = 404,
+            detail = "No se encontró ninguna publicación creada en la base de datos"
+        )
+    return article
+    
+def search_article(id: int) -> dict | None:
+    """
+    Función auxiliar para buscar una publicación por id en la base de datos
+
+    returns:
+        dict: Publicación si se encuentra
+        None: Si la publicación no existe
+    """
+    article = supabase.table("articles").select("*").eq("id", id).execute()
+    return article.data[0] if article.data else None
