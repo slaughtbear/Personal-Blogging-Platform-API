5630558d4ecadbf7cae9300ec39e7ff4689fffd6
slaughtbear <ivanaguirre368@gmail.com>
2025-05-09 17:21:55 -0600

    Add schemas for creation and updating data

--- src/schemas/articles.py
+++ src/schemas/articles.py
@@ -1,9 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import date
+from typing import Optional
 
 class Article(BaseModel):
     id: int
     published_at: date
     title: str
     content: str
-    tags: list[str]
+    tags: list[str] | None
+
+class ArticleCreate(BaseModel):
+    title: str = Field(min_length = 12, max_length = 64)
+    content: str = Field(max_length = 320)
+    tags: Optional[list[str]] = None
+
+class ArticleUpdate(BaseModel):
+    title: Optional[str] = Field(default = None, min_length = 12, max_length = 64)
+    content: Optional[str] = Field(default = None, max_length = 320)
+    tags: Optional[list[str]] = None
