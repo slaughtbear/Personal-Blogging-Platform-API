8662bb80269e5d55eebb64a0373ed5c5aa1685ca
slaughtbear <ivanaguirre368@gmail.com>
2025-05-09 08:08:18 -0600

    Add articles router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from src.routes.articles import articles_router
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"msg": "Welcome to my Personal Blogging Platform API"}
+
+app.include_router(
+    router = articles_router,
+    prefix = "/api/v1/articles",
+    tags = ["v1"]
+)
